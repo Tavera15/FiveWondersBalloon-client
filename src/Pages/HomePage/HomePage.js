baec0d3c05d2e37ae3d35e7360ee43df15ab8554
Tavera15 <joaquinramirez97@yahoo.com>
2024-02-29 23:24:50 -0600

    wwd

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./HomePage.css";
+import "../../images/introservices.jpeg";
+import wwd from "../../images/introservices.jpeg"
+
 
 function HomePage()
 {
@@ -14,7 +17,28 @@ function HomePage()
             </div>
 
             <div className="what-we-do">
-                <h2>We Love What We Do</h2>
+                <h2 className="mb-5">We Love What We Do</h2>
+
+                <div className=" details-box">
+                    <div className="col-0 col-md-1 col-lg-2"></div>
+
+                    <div className="col-12 col-md-5 col-lg-4">
+                        <p className="details-text col-12">
+                            We decorate for all types of events including: 
+                            Birthdays, Graduations, Weddings, Grand Openings, Gender Reveals, Baby Showers, 
+                            Bridal Showers, Retirement Parties, Corporate Events, Milestones, Reunions, and more! 
+                            What kind of event are you having? Give us a call for the best balloon services in Houston!
+                        </p>
+                    </div>
+                    
+                    <div className="col-0 col-md-1 col-lg-1"></div>
+
+                    <div className="col-12 col-md-4 col-lg-3">                    
+                        <img className="col-10" src={wwd} alt="wwd"/>
+                    </div>
+
+                    <div className="col-0 col-md-1 col-lg-2"></div>
+                </div>
             </div>
 
             <div className="specialties">
